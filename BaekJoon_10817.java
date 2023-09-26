9198ff7d6538e62ce06cd60a8c92d0028be24614
BSY3510 <baeksy3510@gmail.com>
2023-09-26 21:21:33 +0900

    BaekJoon codes add

---
+++ BaekJoon_10817.java
@@ -0,0 +1,44 @@
+package test01;
+
+import java.util.Scanner;
+
+public class BaekJoon_10817 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		if (a >= b && a >= c) {
+			if (b > c) {
+				System.out.println(b);
+			}
+			else {
+				System.out.println(c);
+			}
+			
+		}
+		else if (b >= a && b >= c) {
+			if (a > c) {
+				System.out.println(a);
+			}
+			else {
+				System.out.println(c);
+			}
+		}
+		else if (c >= a && c >= b) {
+			if (a > b) {
+				System.out.println(a);
+			}
+			else {
+				System.out.println(b);
+			}
+		}
+		
+		sc.close();
+
+	}
+
+}
